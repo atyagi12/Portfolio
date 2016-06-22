ff3e0c19fd69736a840054e27b29baa6a5c00001
atyagi12 <abhimanyutyagi2@gmail.com>
2016-06-21 20:57:22 -0400

    Uploaded Website source

---
+++ js/script.js
@@ -0,0 +1,62 @@
+
+
+$(document).ready(function(){
+	
+	$(window).stellar();
+	$("#pro-content").hide();
+	$("#hw-content").hide();
+	$("#ot-content").hide();
+	$("#med-content").hide();
+	$("#ibm-content").hide();
+	$("#amel-content").hide();
+	$("#puro-content").hide();
+	$("#game-content").hide();
+	$("#read-content").hide();
+	$("#music-content").hide();
+
+	
+	
+	
+
+	
+
+	
+
+	
+
+	
+
+
+});
+
+function proContent(){
+	$("#pro-content").fadeToggle(400, "linear");
+}
+
+function hwContent(){
+	$("#hw-content").fadeToggle(400, "linear");
+}
+
+function otContent(){
+	$("#ot-content").fadeToggle(400, "linear");
+}
+
+function medContent(){
+	$("#med-content").fadeToggle(400, "linear");
+
+}
+
+function ibmContent(){
+	$("#ibm-content").fadeToggle(400, "linear");
+
+}
+
+function amelContent(){
+	$("#amel-content").fadeToggle(400, "linear");
+
+}
+
+function puroContent(){
+	$("#puro-content").fadeToggle(400, "linear");
+
+}
